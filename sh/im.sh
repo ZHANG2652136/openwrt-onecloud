0456477859a44a31ddf87486cb8d774ec1a1d129
ZHANG2652136 <104855410+ZHANG2652136@users.noreply.github.com>
2024-04-10 10:50:24 +0800

    Update im.sh

--- sh/im.sh
+++ sh/im.sh
@@ -22,25 +22,23 @@ rm -rf feeds/packages/net/mosdns
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-sed -i "s/192.168.1.1/192.168.2.2/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.68.2/" package/base-files/files/bin/config_generate
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 sudo rm -rf package/base-files/files/etc/banner
 
 sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
 
-sed -i "s/%R/by shiyu1314/" package/base-files/files/etc/openwrt_release
 
 
 date=$(date +"%Y-%m-%d")
 echo "                                                    " >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
 echo ".___                               __         .__" >> package/base-files/files/etc/banner
 echo "|   | _____   _____   ____________/  |______  |  |" >> package/base-files/files/etc/banner
 echo "|   |/     \ /     \ /  _ \_  __ \   __\__  \ |  |" >> package/base-files/files/etc/banner
 echo "|   |  Y Y  \  Y Y  (  <_> )  | \/|  |  / __ \|  |__" >> package/base-files/files/etc/banner
 echo "|___|__|_|  /__|_|  /\____/|__|   |__| (____  /____/" >> package/base-files/files/etc/banner
 echo "          \/      \/                        \/      " >> package/base-files/files/etc/banner
-echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
-echo "         %D ${date} by shiyu1314                     " >> package/base-files/files/etc/banner
 echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
 echo "                                                      " >> package/base-files/files/etc/banner
